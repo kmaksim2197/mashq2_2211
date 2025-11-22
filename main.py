486afd9275eabf1cd9af8d89ead0bfc1170ff353
kmaksim2197 <kmaksim2197@gmail.com>
2025-11-22 15:51:32 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+random.randint(1, 6)
+random.randint(1000, 9999)
+random.sample(lst, 2)
+n = random.randint(0, 100); n % 2 == 0
+"".join(random.choice("abcdefghi") for _ in range(10))
+n = random.randint(1, 100); "katta" if n > 50 else n
+random.choice(["heads", "tails"])
+n = random.randint(1, 20); n % 3 == 0
+el = random.choice(lst); lst.remove(el)
+n = random.randint(1, 1000); n * n
